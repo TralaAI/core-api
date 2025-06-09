c83fcdc907d059d2c735ed1ff6f3c6ebd4eb7e8c
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-06-09 16:10:36 +0200

    feat: Refactor TrashDTOController and Program to integrate HolidayApiService for holiday checks

--- Program.cs
+++ Program.cs
@@ -6,21 +6,31 @@ using Api.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// 🪄 Add custom variables for later use.
+var holidayApiKey = builder.Configuration["HolidayApiKey"];
+if (string.IsNullOrWhiteSpace(holidayApiKey))
+    throw new InvalidOperationException("Holiday API key is not configured. Please set the 'HolidayApiKey' in the user secrets.");
+
 // 🛠️ Add services to the container.
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<LitterDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ILitterRepository, LitterRepository>();
-builder.Services.AddScoped<IAggregatedTrashService, AggregatedTrashService>();
-builder.Services.AddHttpClient<IAggregatedTrashService, AggregatedTrashService>();
+builder.Services.AddScoped<IHolidayApiService, HolidayApiService>(provider =>
+{
+    var httpClient = provider.GetRequiredService<IHttpClientFactory>().CreateClient();
+    return new HolidayApiService(holidayApiKey, httpClient);
+});
+builder.Services.AddScoped<IAggregatedTrashService, AggregatedTrashService>(provider =>
+{
+    var httpClient = provider.GetRequiredService<IHttpClientFactory>().CreateClient();
+    return new AggregatedTrashService(httpClient);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
     app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
