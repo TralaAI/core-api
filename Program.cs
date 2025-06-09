609c42f8d1114aab73948a5a17ce629b5eaa4f51
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-06-09 12:01:50 +0200

    refactor: Remove unused authentication and database service registrations for cleanup

--- Program.cs
+++ Program.cs
@@ -8,34 +8,6 @@ var builder = WebApplication.CreateBuilder(args);
 // 🛠️ Add services to the container.
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddTransient<IJwtService, JwtService>();
-builder.Services.AddDbContext<LitterDbContext>(
-    options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-/* TODO: Add options for passwords ect...*/
-);
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(
-                builder.Configuration["Jwt:Key"] ?? throw new InvalidOperationException("JWT Key is not configured.")
-            )
-        )
-    };
-});
-builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
