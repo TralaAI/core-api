b58d1516e5c39b13035b378098c147befde4e5ff
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-06-10 11:54:27 +0200

    fix: Change LitterDbContext to inherit from DbContext instead of IdentityDbContext

--- Data/LitterDbContext.cs
+++ Data/LitterDbContext.cs
@@ -1,10 +1,9 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Api.Models;
 
 namespace Api.Data
 {
-  public class LitterDbContext(DbContextOptions<LitterDbContext> options) : IdentityDbContext(options)
+  public class LitterDbContext(DbContextOptions<LitterDbContext> options) : DbContext(options)
   {
     public DbSet<Litter> Litters { get; set; }
   }
