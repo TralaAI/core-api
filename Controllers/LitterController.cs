30fed64b318689e8acc53f5c813d62cac63d8a44
Daniël van Ginneken <dcjvanginneken@gmail.com>
2025-06-14 21:03:51 +0200

    Implement prediction and retraining methods in LitterController using IFastApiPredictionService

--- Controllers/LitterController.cs
+++ Controllers/LitterController.cs
@@ -1,14 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Api.Models;
 using Api.Interfaces;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace Api.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class LitterController(ILitterRepository litterRepository) : ControllerBase
+public class LitterController(ILitterRepository litterRepository, IFastApiPredictionService fastApiPredictionService) : ControllerBase
 {
     private readonly ILitterRepository _litterRepository = litterRepository;
+    private readonly IFastApiPredictionService _fastApiPredictionService = fastApiPredictionService;
 
     [HttpGet]
     public async Task<ActionResult<List<Litter>>> Get([FromQuery] LitterFilterDto filter)
@@ -19,5 +21,29 @@ public class LitterController(ILitterRepository litterRepository) : ControllerBa
             return NotFound("No litter found matching the filter.");
 
         return Ok(litters);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Predict()
+    {
+        var predictionResult = await _fastApiPredictionService.MakeLitterAmountPredictionAsync(DateTime.UtcNow); // TODO Update to use actual date input
+        if (predictionResult is null)
+            return BadRequest("Prediction failed. Please try again later.");
+
+        if (predictionResult.Prediction is null)
+            return NotFound("No prediction available for the given date.");
+
+        return Ok(predictionResult);
+    }
+
+
+    [HttpPost]
+    public async Task<IActionResult> Retrain()
+    {
+        var predictionResult = await _fastApiPredictionService.RetrainModelAsync();
+        if (predictionResult is false)
+            return StatusCode(StatusCodes.Status500InternalServerError, "Retraining failed. Please try again later.");
+
+        return NoContent();
     }
 }
--- Interfaces/IFastApiPredictionService.cs
+++ Interfaces/IFastApiPredictionService.cs
@@ -6,6 +6,7 @@ namespace Api.Interfaces
     {
         Task<PredictionResponse?> MakeLitterAmountPredictionAsync(DateTime date);
 
+        [Obsolete("Retraining the model is not supported in the current version. This method will be added in a future release.")]
         Task<bool> RetrainModelAsync();
 
     }
